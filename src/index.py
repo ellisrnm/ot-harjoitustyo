55ab75b8b4a88197ad260a82b023ac502adb3c03
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2021-11-23 19:22:25 +0200

    Add commands to app

--- src/index.py
+++ src/index.py
@@ -14,6 +14,9 @@ class BugTrackerApp():
         print("0: Lopetus")
         print("1: Tulosta nykyiset projektit")
         print("2: Luo uusi projekti")
+        print("3: Tulosta projektin aliprojektit")
+        print("4: Luo uusi aliprojekti")
+        print("5: Raportoi uusi bugi")
 
         while True:
             print("")
@@ -26,6 +29,27 @@ class BugTrackerApp():
             elif command == "2":
                 project_name = input("Nimeä projekti: ")
                 self.create_project(project_name)
+            elif command == "3":
+                project_name = input("Valitse ensin projekti: ")
+                for project in self.__projects:
+                    if project.name==project_name:
+                        for subproject in project.subprojects:
+                            print(subproject.name)
+            elif command == "4":
+                project_name = input("Valitse ensin projekti: ")
+                subproject_name = input("Nimeä aliprojekti: ")
+                for project in self.__projects:
+                    if project.name==project_name:
+                        project.create_subproject(subproject_name)
+            elif command == "5":
+                project_name = input("Valitse ensin projekti: ")
+                subproject_name = input("Valitse vielä aliprojekti: ")
+                bug_name = input("Nimeä löytynyt bugi: ")
+                for project in self.__projects:
+                    if project.name==project_name:
+                        for subproject in project.subprojects:
+                            if subproject.name==subproject_name:
+                                subproject.report_bug(bug_name)
             else:
                 print("Komento ei käytössä")
 
