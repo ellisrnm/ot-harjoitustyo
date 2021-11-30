d90f761e4b26d907b441a6338117ea677e8c7791
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2021-11-30 22:05:14 +0200

    Add tests

--- src/tests/project_test.py
+++ src/tests/project_test.py
@@ -1,5 +1,5 @@
 import unittest
-from project import Project
+from project import Project, SubProject, Bug
 
 class TestProject(unittest.TestCase):
     def setUp(self):
@@ -7,3 +7,23 @@ class TestProject(unittest.TestCase):
 
     def test_created_project_has_no_subprojects(self):
         self.assertEqual(self.project.subprojects,[])
+
+    def test_creating_subproject_increases_subproject_list_length(self):
+        self.project.create_subproject('P_SP1')
+        self.assertEqual(len(self.project.subprojects),1)
+
+class TestSubProject(unittest.TestCase):
+    def setUp(self):
+        self.subproject = SubProject("SP1")
+
+    def test_created_project_has_no_reported_bugs(self):
+        self.assertEqual(self.subproject.bugs,[])
+
+    def test_reporting_bugs_increases_bug_list_length(self):
+        self.subproject.report_bug("BUG1")
+        self.subproject.report_bug("BUG2")
+        self.assertEqual(len(self.subproject.bugs),2)
+
+class TestBug(unittest.TestCase):
+    def setUp(self):
+        self.bug = Bug("New bug", "Missing data", "Low")
